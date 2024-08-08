5fa6541429e76f035d2839cba552eb5253656895
Bhuvanesh Abburi <bhuvanesh.msu@gmail.com>
2024-08-08 16:26:01 -0500

    - Added files for initial commit

---
+++ src/config.py
@@ -0,0 +1,5 @@
+app_config = {
+    "title": "Analytics Dashboard API",
+    "version": "1.0.0",
+    "docs_url": "/analytics_dashboard",
+}
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+from config import app_config
+
+app = FastAPI(**app_config)
+
+@app.get("/", include_in_schema=True)
+async def root():
+    return {"Health":"OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8083)
